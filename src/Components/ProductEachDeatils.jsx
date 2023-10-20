e54b0890c726381f5f466d4d6b531b21663ecb6a
Souravn1200 <souravn1200@gmail.com>
2023-10-21 00:28:47 +0600

    Cart data sending through api and showing swal

--- src/Components/ProductEachDeatils.jsx
+++ src/Components/ProductEachDeatils.jsx
@@ -1,35 +1,80 @@
 
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import swal from 'sweetalert';
 
 const ProductEachDeatils = () => {
     const id = useParams();
+    // const myData = useLoaderData();
+    const {_id,name, brand, type, photo, price, description, rating} = useLoaderData();
 
-const myData = useLoaderData();
+
+const handleAddToCart = async () =>{
+    const cart = {_id,name, brand, type, photo, price, description, rating};
+    await fetch('http://localhost:5000/cart', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(cart)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+
+        if (data.insertedId) {
+            swal("Done!", "Your Product was added to the Cart", "success")
+        }
+       
+    })
+
+
+}
 
     return (
         <div>
+
+            {/* {console.log(myData)} */}
         {
-            myData.map(data => (
-                <div key={data._id}>
-                    <>
-                    <div className="card card-compact mx-auto bg-base-100  mt-8">
-  <figure><img src={data.photo} alt="Shoes" className='w-[900px] h-[400px] rounded'/></figure>
-  <div className="card-body mx-28">
-    <h2 className="card-title">{data.name}</h2>
-    <p>{data.description}</p>
-    <p><span className='font-bold'>Type</span> : {data.type}</p>
-    <p><span className='font-bold'>Price</span> : {data.price}</p>
-    <p><span className='font-bold'>Rating</span> : {data.rating}</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-success">Add to cart</button>
+//             myData.map(data => (
+//                 <div key={data._id}>
+//                     <>
+//                     <div className="card card-compact mx-auto bg-base-100  mt-8">
+//   <figure><img src={data.photo} alt="Shoes" className='w-[900px] h-[400px] rounded'/></figure>
+//   <div className="card-body mx-28">
+//     <h2 className="card-title">{data.name}</h2>
+//     <p>{data.description}</p>
+//     <p><span className='font-bold'>Type</span> : {data.type}</p>
+//     <p><span className='font-bold'>Price</span> : {data.price}</p>
+//     <p><span className='font-bold'>Rating</span> : {data.rating}</p>
+//     <div className="card-actions justify-end">
+//       <button className="btn btn-success" onClick={handleAddToCart}>Add to cart</button>
       
-    </div>
+//     </div>
+//   </div>
+// </div>
+//                     </>
+//                 </div>
+//             ))
+
+
+
+
+<div className="card card-compact mx-auto bg-base-100  mt-8">
+<figure><img src={photo} alt="Shoes" className='w-[900px] h-[400px] rounded'/></figure>
+<div className="card-body mx-28">
+  <h2 className="card-title">{name}</h2>
+  <p>{description}</p>
+  <p><span className='font-bold'>Type</span> : {type}</p>
+  <p><span className='font-bold'>Price</span> : {price}</p>
+  <p><span className='font-bold'>Rating</span> : {rating}</p>
+  <div className="card-actions justify-end">
+    <button className="btn btn-success" onClick={handleAddToCart}>Add to cart</button>
+    
   </div>
 </div>
-                    </>
-                </div>
-            ))
+</div>
+
         }
     </div>
     );
